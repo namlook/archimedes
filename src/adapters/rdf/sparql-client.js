8a2fa6ffa73e6b2d4657cf62be1a3d9b050859fd
namlook <nicolas.clairon@elkorado.com>
2015-10-21 15:45:46 +0200

    replace request by wreck

--- src/adapters/rdf/sparql-client.js
+++ src/adapters/rdf/sparql-client.js
@@ -1,66 +1,59 @@
 
-import request from 'request';
+import wreck from 'wreck';
 import querystring from 'querystring';
 import _ from 'lodash';
 
 import Promise from 'bluebird';
 
 let sparqlClient = function(endpoint) {
-    let requestDefaults = {
-        url: endpoint,
-        method: 'POST',
-        encoding: 'utf8',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Accept': 'application/sparql-results+json'
-            // 'Accept': 'application/json'
-        }
-    };
 
     return {
         execute(sparql) {
-            let queryOperators = ['select', 'construct', 'describe', 'ask'];
-            let isQuery = _.compact(queryOperators.map((op) => {
-                return _.startsWith(_.trim(sparql.toLowerCase()), op);
-            })).length;
-
-
-            let body;
-            if (isQuery) {
-                body = {query: sparql};
-            } else {
-                body = {update: sparql};
-            }
-
-            // console.log('sparql>>>>>>>');
-            // console.log(sparql);
-            // console.log('<<<<<<<<<<sparql');
-
-            let requestOptions = Object.assign({}, requestDefaults, {
-                body: querystring.stringify(body)
-            });
             return new Promise((resolve, reject) => {
-                process.nextTick(function() {
-                    request.post(requestOptions, (err, response, resbody) => {
-                        if (err) {
-                            return reject(err);
-                        }
+                let queryOperators = ['select', 'construct', 'describe', 'ask'];
+                let isQuery = _.compact(queryOperators.map((op) => {
+                    return _.startsWith(_.trim(sparql.toLowerCase()), op);
+                })).length;
 
-                        if (response.statusCode >= 400) {
-                            return reject(resbody);
-                        }
 
-                        process.nextTick(function() {
+                let body;
+                if (isQuery) {
+                    body = {query: sparql};
+                } else {
+                    body = {update: sparql};
+                }
 
-                            let data;
-                            try {
-                                data = JSON.parse(resbody);
-                            } catch (e) {
-                                data = undefined;
-                            }
-                            resolve(data);
-                        });
-                    });
+                // console.log('sparql>>>>>>>');
+                // console.log(sparql);
+                // console.log('<<<<<<<<<<sparql');
+
+                let options = {
+                    headers: {
+                        'content-type': 'application/x-www-form-urlencoded',
+                        'accept': 'application/sparql-results+json'
+                    },
+                    payload: querystring.stringify(body)
+                };
+
+                wreck.post(endpoint, options, (err, response, payload) => {
+                    payload = payload.toString();
+
+                    if (err) {
+                        return reject(err);
+                    }
+
+                    if (response.statusCode >= 400) {
+                        return reject(payload);
+                    }
+
+
+                    let data;
+                    try {
+                        data = JSON.parse(payload);
+                    } catch (e) {
+                        data = undefined;
+                    }
+                    resolve(data);
                 });
             });
         }
