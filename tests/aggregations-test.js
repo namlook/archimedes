ac77922e3f0c5ef18ebcb0d2abcadd3d3ce69c25
namlook <nicolas.clairon@elkorado.com>
2015-11-24 15:02:31 +0100

    add aggregation tests

---
+++ tests/aggregations-test.js
@@ -0,0 +1,119 @@
+
+import 'source-map-support/register';
+
+import Lab from 'lab';
+var lab = exports.lab = Lab.script();
+
+import Code from 'code';
+var describe = lab.describe;
+var it = lab.it;
+var before = lab.before;
+var expect = Code.expect;
+
+import testQueries from './aggregations';
+import loadDb from './data';
+import chalk from 'chalk';
+import _ from 'lodash';
+import {inspect} from 'util';
+
+
+var processTest = function(db, testQuery) {
+
+    return new Promise((resolve, reject) => {
+
+        if (!testQuery) {
+            return resolve();
+        }
+
+        let promise = db.aggregate(testQuery.model, testQuery.aggregation, testQuery.query, testQuery.options);
+
+        if (testQuery.error != null) {
+
+            promise.catch((error) => {
+                try {
+                    expect(error.message).to.equal(testQuery.error);
+                    if (_.has(error, 'extra.0.message')) {
+                        expect(error.extra[0].message).to.equal(testQuery.errorExtraMessage);
+                    }
+                } catch (e) {
+                    console.log('------------------');
+                    console.log('>>>', chalk.blue(inspect(testQuery, {depth: 10, colors: true})));
+                    console.log('------------------');
+                    console.log('>>>', chalk.red(inspect(e, {depth: 10, colors: true})));
+                    console.log('------------------');
+                    console.log(error.stack);
+                    return reject(e);
+                }
+                return resolve();
+            });
+
+        } else {
+
+            promise.then((results) => {
+                try {
+                    if (testQuery.results) {
+                        expect(results).to.deep.equal(testQuery.results);
+                    }
+                } catch(e) {
+                    console.log('------------------');
+                    console.log('expected>', chalk.blue(inspect(testQuery, {depth: 10, colors: true})));
+                    console.log('------------------');
+                    console.log('actual>', chalk.red(inspect(results, {depth: 10, colors: true})));
+                    console.log('------------------');
+                    return reject(e);
+                }
+
+                return resolve();
+            }).catch((error) => {
+                console.log('xxxx', testQuery);
+                console.log(inspect(error, {depth: 10}));
+                console.log(error.stack);
+                return reject(error);
+            });
+
+        }
+
+    });
+};
+
+
+describe('#aggregate()', function(){
+
+    var db;
+    before(function(done) {
+        loadDb().then((registeredDB) => {
+            db = registeredDB;
+            done();
+        }).catch((error) => {
+            console.log(error);
+            console.log(error.stack);
+        });
+    });
+
+
+    var testFn = function(testQuery) {
+        return function(done) {
+            processTest(db, testQuery).then(() => {
+                done();
+            }).catch((error) => {
+                console.log(error);
+            });
+        };
+    };
+
+
+    for (let i = 0; i < testQueries.length; i++) {
+        let testQuery = testQueries[i];
+        let testLauncher = it;
+        if (testQuery.only) {
+            testLauncher = it.only;
+        }
+        if (testQuery.skip) {
+            testLauncher = it.skip;
+        }
+        testLauncher(`${testQuery.model}: ${testQuery.should}`, {parallel: false}, testFn(testQuery));
+    }
+});
+
+
+
---
+++ tests/aggregations.js
@@ -0,0 +1,236 @@
+
+export default [
+
+    {
+        should: 'make simple aggregation',
+        model: 'User',
+        aggregation: {label: 'gender', total: {$count: 'gender'}},
+        query: {gender: 'male'},
+        results: [
+            { label: 'male', total: 3 }
+        ]
+    },
+    {
+        should: 'make simple aggregation with query',
+        model: 'User',
+        aggregation: {x: 'gender', y: {$count: 'gender'}},
+        query: {gender: 'male'},
+        results: [
+            { x: 'male', y: 3 }
+        ]
+    },
+    {
+        should: 'accept true for the $count operator',
+        model: 'User',
+        aggregation: {x: 'gender', y: {$count: true}},
+        results: [
+            { x: 'male', y: 3},
+            { x: 'female', y: 2 }
+        ]
+    },
+
+    /** group by types **/
+    // {
+    //     model: 'Content',
+    //     groupBy: '_type',
+    //     results: [
+    //     ...
+    //     ]
+    // },
+
+    /** group by deep relation **/
+    {
+        should: 'aggregate on deep relations',
+        model: 'BlogPost',
+        aggregation: {gender: 'author.gender', total: {$count: 'author'}},
+        results: [
+            { gender: 'male', total: 6},
+            { gender: 'female', total: 4 }
+        ]
+    },
+    {
+        should: 'aggregate on deep relations with query',
+        model: 'BlogPost',
+        aggregation: {x: 'author.gender', y: {$count: true}},
+        query: {'author.gender': 'male'},
+        results: [
+            { x: 'male', y: 6 }
+        ]
+    },
+    {
+        should: 'aggregate on deep relations with query and operator',
+        model: 'BlogPost',
+        aggregation: {gender: 'author.gender', nbSubscriptions: {$count: 'author.subscribedMailingList'}},
+        query: {'author.gender': 'female', 'author.subscribedMailingList': true},
+        results: [
+            { gender: 'female', nbSubscriptions: 4 }
+        ]
+    },
+    /** group by deep relation with avg operator on ratting **/
+    {
+        should: 'accept $avg operator',
+        model: 'BlogPost',
+        aggregation: {
+            gender: 'author.gender',
+            ratting: {$avg: 'ratting'}
+        },
+        results: [
+            { gender: 'male', ratting: 2.5 },
+            { gender: 'female', ratting: 1.5 }
+        ]
+    },
+
+    /** max operator **/
+    {
+        should: 'accept $max operator',
+        model: 'BlogPost',
+        aggregation: {
+            gender: 'author.gender',
+            rattingMax: {$max: 'ratting'}
+        },
+        results: [
+            { gender: 'male', rattingMax: 5},
+            { gender: 'female', rattingMax: 3 }
+        ]
+    },
+
+    /** group by multiple properties **/
+    {
+        should: 'sort the results by label',
+        model: 'BlogPost',
+        aggregation: {
+            isPublished: 'isPublished',
+            gender: 'author.gender',
+            total: {$count: 'isPublished'}
+        },
+        options: {sort: ['isPublished', 'gender']},
+        results: [
+            { isPublished: 'false', gender: 'female', total: 2},
+            { isPublished: 'false', gender: 'male', total: 2},
+            { isPublished: 'true', gender: 'female', total: 2},
+            { isPublished: 'true', gender: 'male', total: 4}
+        ]
+    },
+    {
+        should: 'sort the results by label in inversed order',
+        model: 'BlogPost',
+        aggregation: {
+            isPublished: 'isPublished',
+            gender: 'author.gender',
+            total: {$count: 'isPublished'}
+        },
+        options: {sort: ['-total', 'isPublished', 'gender']},
+        results: [
+            { isPublished: 'true', gender: 'male', total: 4},
+            { isPublished: 'false', gender: 'female', total: 2},
+            { isPublished: 'false', gender: 'male', total: 2},
+            { isPublished: 'true', gender: 'female', total: 2}
+        ]
+    },
+    {
+        should: 'sort the results by multiple labels',
+        model: 'BlogPost',
+         aggregation: {
+            isPublished: 'isPublished',
+            gender: 'author.gender',
+            total: {$avg: 'ratting'}
+        },
+        options: {sort: ['isPublished', 'gender']},
+        results: [
+            { isPublished: 'false', gender: 'female', total: 1.5},
+            { isPublished: 'false', gender: 'male', total: 1.5},
+            { isPublished: 'true', gender: 'female', total: 1.5},
+            { isPublished: 'true', gender: 'male', total: 3}
+        ]
+    },
+    {
+        should: 'sort the results by multiple labels with query',
+        model: 'BlogPost',
+         aggregation: {
+            isPublished: 'isPublished',
+            gender: 'author.gender',
+            total: {$avg: 'ratting'}
+        },
+        query: {isPublished: {$exists: true}},
+        options: {sort: ['isPublished', 'gender']},
+        results: [
+            { isPublished: 'false', gender: 'female', total: 1.5},
+            { isPublished: 'false', gender: 'male', total: 1.5},
+            { isPublished: 'true', gender: 'female', total: 1.5},
+            { isPublished: 'true', gender: 'male', total: 3}
+        ]
+    },
+
+    /*** limit ***/
+    {
+        should: 'limit the number of results',
+        model: 'BlogPost',
+        aggregation: {
+            isPublished: 'isPublished',
+            gender: 'author.gender',
+            total: {$count: 'isPublished'}
+        },
+        options: {limit: 2, sort: ['isPublished', 'gender']},
+        results: [
+            { isPublished: 'false', gender: 'female', total: 2},
+            { isPublished: 'false', gender: 'male', total: 2}
+        ]
+    },
+
+    /*** errors ***/
+    {
+        should: 'throw an error if the operator is unknown',
+        model: 'BlogPost',
+        aggregation: {
+            gender: 'author.gender',
+            ratting: {$unknownOperator: 'ratting'}
+        },
+        error: 'aggregate: unknown operator "$unknownOperator"'
+    },
+    {
+        should: 'throw an error if the property is unknown',
+        model: 'BlogPost',
+        aggregation: {
+            gender: 'author.gender',
+            ratting: {$avg: 'unknownProperty'}
+        },
+        error: 'aggregate: unknown property "unknownProperty" for model "BlogPost"'
+    },
+    {
+        should: 'throw an error when sorting with an unknown variable',
+        model: 'BlogPost',
+        aggregation: {
+            gender: 'author.gender',
+            ratting: {$avg: 'ratting'}
+        },
+        options: {sort: ['unknownVariable']},
+        error: 'aggregate: unknown sorting constraint "unknownVariable"'
+    }
+    // TODO to be implemented
+    // {
+    //     model: 'Thing',
+    //     groupBy: {
+    //         property: ['_type', 'gender']
+    //     },
+    //     results: [
+    //         {
+    //             label: 'Comment',
+    //             value: [
+    //                 {label: 'male', value: 16},
+    //                 {label: 'female', value: 10}
+    //             ]
+    //         },
+    //         {
+    //             label: 'BlogPost',
+    //             value: [
+    //                 {label: 'male', value: 6},
+    //                 {label: 'female', value: 4}
+    //             ]
+    //         },
+    //         {
+    //             label: 'Book',
+    //             value: [...]
+    //         }
+    //     ]
+    // }
+];
