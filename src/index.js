eb6267ccfed91bbb7c646587bb18d6cee26b32fe
namlook <nicolas.clairon@elkorado.com>
2016-02-12 17:26:41 +0100

    generate endpoint from engine

--- src/index.js
+++ src/index.js
@@ -4,18 +4,43 @@ import rdfAdapter from './adapters/rdf';
 import memoryAdapter from './adapters/memory';
 
 export var triplestore = function(config) {
+    if (!config.engine) {
+        throw new Error('archimedes: engine not found');
+    }
+
+    let baseUri;
+    if(!config.port) {
+        baseUri = `http://${config.host}`;
+    } else {
+        baseUri = `http://${config.host}:${config.port}`;
+    }
+
+    let endpoint;
+    switch (config.engine) {
+
+        case 'virtuoso':
+            endpoint = `${baseUri}/sparql`;
+            break;
+
+        case 'blazegraph':
+            endpoint = `${baseUri}/bigdata/sparql`;
+            break;
+
+        default:
+            throw new Error(`archimedes: unknown engine "${config.engine}"`);
+    }
+
+
     let rdf = rdfAdapter({
         graphUri: config.graphUri,
-        // host: config.host,
-        // port: config.port,
-        endpoint: config.endpoint
+        endpoint: endpoint
     });
 
-    return database(rdf);
+    return database(rdf, config);
 };
 
-export var memory = function() {
-    return database(memoryAdapter());
+export var memory = function(config) {
+    return database(memoryAdapter(), config);
 };
 
 export default database;
