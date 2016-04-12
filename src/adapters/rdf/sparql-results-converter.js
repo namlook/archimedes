d00263398cc605584b7644c1881f119b4d447fae
namlook <nicolas.clairon@elkorado.com>
2016-04-12 14:25:53 +0200

    fix eslint issues

--- src/adapters/rdf/sparql-results-converter.js
+++ src/adapters/rdf/sparql-results-converter.js
@@ -3,31 +3,30 @@ import _ from 'lodash';
 
 import rdfUtilities from './rdf-utils';
 
-module.exports = function(db, modelName, query) {
-
+module.exports = function main(db, modelName, _query) {
     /** don't work directly on the query **/
-    query = _.cloneDeep(query);
+    const query = _.cloneDeep(_query);
 
     const rdfUtils = rdfUtilities(db);
     const internals = {};
 
     const fieldProperties = _.toPairs(query.field)
-        .map(([fieldName, fieldInfos]) => {
-            let isArray = _.isArray(fieldInfos);
-            fieldInfos = isArray && fieldInfos[0] || fieldInfos;
+        .map(([fieldName, _fieldInfos]) => {
+            const isArray = _.isArray(_fieldInfos);
+            let fieldInfos = isArray && _fieldInfos[0] || _fieldInfos;
 
             if (_.isString(fieldInfos)) {
-                fieldInfos = {$property: fieldInfos};
+                fieldInfos = { $property: fieldInfos };
             }
 
             let propertyName = fieldInfos.$property;
 
-            let optional = _.includes(propertyName, '?');
+            const optional = _.includes(propertyName, '?');
             if (optional) {
                 propertyName = propertyName.split('?').join('');
             }
 
-            if(_.endsWith(propertyName, '._id')) {
+            if (_.endsWith(propertyName, '._id')) {
                 propertyName = propertyName.split('.').slice(0, -1).join('.');
             }
 
@@ -36,25 +35,26 @@ module.exports = function(db, modelName, query) {
                 propertyName,
                 array: isArray,
                 fields: fieldInfos.$fields,
-                propertyRaw: fieldInfos.$property.split('?').join('')
+                propertyRaw: fieldInfos.$property.split('?').join(''),
             };
         });
 
-    const aggregationProperties =  _.toPairs(query.aggregate)
-        .map(([fieldName, aggregationInfos]) => {
+    const aggregationProperties = _.toPairs(query.aggregate)
+        .map(([fieldName, _aggregationInfos]) => {
+            let aggregationInfos = _aggregationInfos;
             if (!aggregationInfos.$aggregator) {
-                let [aggregator, propertyName] = _.toPairs(aggregationInfos)[0];
-                aggregator = aggregator.slice(1);
+                const [_aggregator, propertyName] = _.toPairs(aggregationInfos)[0];
+                const aggregator = _aggregator.slice(1);
                 aggregationInfos = {
                     $aggregator: aggregator,
-                    $property: propertyName
+                    $property: propertyName,
                 };
             } else if (!aggregationInfos.$property) {
-                let aggregator = aggregationInfos.$aggregator;
+                const aggregator = aggregationInfos.$aggregator;
                 if (aggregator === 'count') {
                     aggregationInfos.$property = true;
                 } else if (aggregator === 'array' && aggregationInfos.$fields) {
-                    let propertyName = _.values(aggregationInfos.$fields)[0].split('.')[0];
+                    const propertyName = _.values(aggregationInfos.$fields)[0].split('.')[0];
                     aggregationInfos.$property = propertyName;
                 }
             }
@@ -65,14 +65,14 @@ module.exports = function(db, modelName, query) {
             }
 
             let propertyName = aggregationInfos.$property;
-            let aggregator = aggregationInfos.$aggregator.toLowerCase();
+            const aggregator = aggregationInfos.$aggregator.toLowerCase();
 
-            let optional = _.includes(propertyName, '?');
+            const optional = _.includes(propertyName, '?');
             if (optional) {
                 propertyName = propertyName.split('?').join('');
             }
 
-            if(_.endsWith(propertyName, '._id')) {
+            if (_.endsWith(propertyName, '._id')) {
                 propertyName = propertyName.split('.').slice(0, -1).join('.');
             }
 
@@ -82,7 +82,7 @@ module.exports = function(db, modelName, query) {
                 aggregator,
                 array: aggregator === 'array',
                 fields: aggregationInfos.$fields,
-                propertyRaw: aggregationInfos.$property.split('?').join('')
+                propertyRaw: aggregationInfos.$property.split('?').join(''),
             };
         });
 
@@ -91,10 +91,10 @@ module.exports = function(db, modelName, query) {
     internals.PROPERTIES_BY_FIELDNAME = _.keyBy(internals.PROPERTIES, 'fieldName');
 
     internals.SORTED_FIELDS = _.keyBy(
-        _.get(query, 'sort', []).map((fieldName) => {
-            let descOrder = _.startsWith(fieldName, '-');
-            fieldName = descOrder ? fieldName.slice(1) : fieldName;
-            let order = descOrder ? 'desc' : 'asc';
+        _.get(query, 'sort', []).map((_fieldName) => {
+            const descOrder = _.startsWith(_fieldName, '-');
+            const fieldName = descOrder ? _fieldName.slice(1) : _fieldName;
+            const order = descOrder ? 'desc' : 'asc';
             return { fieldName, order };
         }),
         'fieldName'
@@ -103,78 +103,74 @@ module.exports = function(db, modelName, query) {
     // console.log('PROPERTIES>', internals.PROPERTIES);
     // console.log('PROPERTIES_BY_FIELDNAME>', internals.PROPERTIES_BY_FIELDNAME);
 
-    internals._decodeURIValue = function(_fieldName, iriValue, parent) {
+    internals._decodeURIValue = (_fieldName, iriValue, parent) => {
         if (_.isPlainObject(iriValue)) {
-
-            let decodedPairs = _.toPairs(iriValue).map((pair) => {
-                return [
-                    pair[0],
-                    internals._decodeURIValue(pair[0], pair[1], _fieldName)
-                ];
-            });
+            const decodedPairs = _.toPairs(iriValue).map((pair) =>
+                [pair[0], internals._decodeURIValue(pair[0], pair[1], _fieldName)]
+            );
             return _.fromPairs(decodedPairs);
+        }
 
-        } else {
-            let decodedValue = decodeURIComponent(iriValue);
+        const decodedValue = decodeURIComponent(iriValue);
 
-            let propertyRaw, propertyName;
-            if (parent) {
-                let propInfos = internals.PROPERTIES_BY_FIELDNAME[parent];
-                if (propInfos.fields) {
-                    propertyRaw = propInfos.fields[_fieldName];
+        let propertyRaw;
+        let propertyName;
+        if (parent) {
+            const propInfos = internals.PROPERTIES_BY_FIELDNAME[parent];
+            if (propInfos.fields) {
+                propertyRaw = propInfos.fields[_fieldName];
 
-                    /** process embeded field's values in object **/
-                    if (_.endsWith(propertyRaw, '._id')) {
-                        propertyName = propertyRaw.split('._id')[0];
-                    } else if (_.endsWith(propertyRaw, '._type')) {
-                        return db.rdfClasses2ModelNameMapping[decodedValue]
-                    } else {
-                        propertyName = propertyRaw;
-                    }
-
+                /** process embeded field's values in object **/
+                if (_.endsWith(propertyRaw, '._id')) {
+                    propertyName = propertyRaw.split('._id')[0];
+                } else if (_.endsWith(propertyRaw, '._type')) {
+                    return db.rdfClasses2ModelNameMapping[decodedValue];
                 } else {
-                    propertyRaw = propInfos[_fieldName];
                     propertyName = propertyRaw;
                 }
             } else {
-                let propInfos = internals.PROPERTIES_BY_FIELDNAME[_fieldName];
-                propertyRaw = propInfos.propertyRaw;
-                propertyName = propInfos.propertyName;
+                propertyRaw = propInfos[_fieldName];
+                propertyName = propertyRaw;
             }
-
-            if (_.endsWith(propertyRaw, '._id')) {
-                propertyName = propertyName.split('?').join('');
-                const property = db[modelName].schema.getProperty(propertyName);
-                return rdfUtils.rdfURI2id(property.type, decodedValue);
-            }
-
-            return decodedValue;
+        } else {
+            const propInfos = internals.PROPERTIES_BY_FIELDNAME[_fieldName];
+            propertyRaw = propInfos.propertyRaw;
+            propertyName = propInfos.propertyName;
         }
+
+        if (_.endsWith(propertyRaw, '._id')) {
+            propertyName = propertyName.split('?').join('');
+            const property = db[modelName].schema.getProperty(propertyName);
+            return rdfUtils.rdfURI2id(property.type, decodedValue);
+        }
+
+        return decodedValue;
     };
 
-    internals.rdfValuesConvertor = function(fieldName){
-        return {
-            'number': parseFloat,
-            'date': (value) => new Date(value),
-            'boolean': (value) => _(['true', '1', 1, 'yes']).includes(value),
-            'string': function(value) {
-                let propertyInfos = internals.PROPERTIES_BY_FIELDNAME[fieldName];
-                let propertyName = propertyInfos.propertyName;
+    internals.rdfValuesConvertor = (fieldName) => (
+        {
+            number: parseFloat,
+            date: (value) => new Date(value),
+            boolean: (value) => _(['true', '1', 1, 'yes']).includes(value),
+            string: (_value) => {
+                const propertyInfos = internals.PROPERTIES_BY_FIELDNAME[fieldName];
+                const propertyName = propertyInfos.propertyName;
+                let value = _value;
                 if (propertyInfos.array) {
                     value = JSON.parse(value).map(
                         (val) => internals._decodeURIValue(fieldName, val)
                     );
                     value = _.compact(value);
 
-                    /*** always sort the array ***/
-                    let property = db[modelName].schema.getProperty(propertyName);
+                    /** always sort the array **/
+                    const property = db[modelName].schema.getProperty(propertyName);
 
-                    value = _.sortBy(value)
+                    value = _.sortBy(value);
                     if (property.isRelation() && value.length && value[0]._id) {
                         value = _.sortBy(value, '_id');
                     }
 
-                    let sortOrder = _.get(internals.SORTED_FIELDS, `${fieldName}.order`);
+                    const sortOrder = _.get(internals.SORTED_FIELDS, `${fieldName}.order`);
                     if (sortOrder === 'desc') {
                         value = _.reverse(value);
                     }
@@ -182,23 +178,23 @@ module.exports = function(db, modelName, query) {
 
                 return value;
             },
-            'iri': function(value) {
+            iri: (value) => {
                 const propertyInfos = internals.PROPERTIES_BY_FIELDNAME[fieldName];
-                let propertyName = propertyInfos.propertyName;
+                const propertyName = propertyInfos.propertyName;
 
                 if (_.endsWith(propertyName, '._type')) {
-                    return db.rdfClasses2ModelNameMapping[value]
+                    return db.rdfClasses2ModelNameMapping[value];
                 }
 
                 const property = db[modelName].schema.getProperty(propertyName);
                 return rdfUtils.rdfURI2id(property.type, value);
-            }
+            },
         }
-    };
+    );
 
-    internals.buildValueFromRdf = function(fieldName, rdfInfo) {
-        let convertor = internals.rdfValuesConvertor(fieldName);
-        let datatype = rdfInfo.type === 'literal' ? rdfInfo.datatype : 'iri';
+    internals.buildValueFromRdf = (fieldName, rdfInfo) => {
+        const convertor = internals.rdfValuesConvertor(fieldName);
+        const datatype = rdfInfo.type === 'literal' ? rdfInfo.datatype : 'iri';
 
         let valueType;
         if (!datatype) {
@@ -214,10 +210,10 @@ module.exports = function(db, modelName, query) {
 
     return {
         convert(item) {
-            const findFieldNameFromProperty = function(propertyName) {
-                let propInfo = _.find(internals.PROPERTIES, function(o) {
-                    return o.propertyName === propertyName && !o.aggregator;
-                });
+            const findFieldNameFromProperty = (propertyName) => {
+                const propInfo = _.find(internals.PROPERTIES, (o) =>
+                    o.propertyName === propertyName && !o.aggregator
+                );
                 return propInfo ? propInfo.fieldName : null;
             };
 
@@ -228,27 +224,27 @@ module.exports = function(db, modelName, query) {
                 .toPairs()
                 .map(([fieldName, rdfInfo]) => {
                     if (fieldName === _idFieldName) {
-                        let uri = rdfInfo.value;
+                        const uri = rdfInfo.value;
                         return [
                             _idFieldName,
-                            rdfUtils.rdfURI2id(modelName, uri)
+                            rdfUtils.rdfURI2id(modelName, uri),
                         ];
                     }
 
                     if (fieldName === _typeFieldName) {
-                        let typeUri = item[_typeFieldName].value;
-                        let typeValue = db.rdfClasses2ModelNameMapping[typeUri];
+                        const typeUri = item[_typeFieldName].value;
+                        const typeValue = db.rdfClasses2ModelNameMapping[typeUri];
                         return [_typeFieldName, typeValue];
                     }
 
-                    fieldName = fieldName.split(rdfUtils.RELATION_SEPARATOR).join('.');
-                    let value = internals.buildValueFromRdf(fieldName, rdfInfo);
+                    const newFieldName = fieldName.split(rdfUtils.RELATION_SEPARATOR).join('.');
+                    const value = internals.buildValueFromRdf(newFieldName, rdfInfo);
 
                     if (value === '') {
-                        return;
+                        return undefined;
                     }
 
-                    return [fieldName, value];
+                    return [newFieldName, value];
                 })
                 .compact()
                 .fromPairs()
@@ -258,7 +254,6 @@ module.exports = function(db, modelName, query) {
 
             doc = unflatten(doc);
             return _.omitBy(doc, (o) => _.isArray(o) && _.isEmpty(o));
-
-        }
+        },
     };
 };
